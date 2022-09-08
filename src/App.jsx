8bef93a9b4087e23a8e16a3eadec505f365b99dd
Alexis Matos <alexisxzinho@hotmail.com>
2022-09-08 12:56:02 +0200

    updating site

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/white_logo.png'
+import Card from '../../UI/Card'
+
+//icons
+import { MdOutlineSick } from 'react-icons/md'
+import { TbBeach } from 'react-icons/tb'
+import { AiOutlineFieldTime } from 'react-icons/ai'
 
 //pages
 import Home from './pages/home/Home'
@@ -11,7 +18,37 @@ import En from './pages/en/En'
 const App = () => {
     return (
         <>
-            <Home />
+            <div className="de">
+                <Link to='/' className="logo">
+                    <img src={Logo} alt="Nav Logo" />
+                </Link>
+                <div className="container de__container">
+                    <h2>Anfragen</h2>
+                    <div className="de__container-requests">
+                        <Card className={'contact__card'}>
+                            <MdOutlineSick className='contact__option-icon' />
+                            <h4>Krank</h4>
+                            <h5>Formular</h5>
+                            <a href="https://forms.gle/QzPSL8MagS2b7BG56" target="__blank">Füllen</a>
+                        </Card>
+
+                        <Card className={'contact__card'}>
+                            <TbBeach className='contact__option-icon' />
+                            <h4>Urlaub</h4>
+                            <h5>Formular</h5>
+                            <a href="/en" target="__blank">Füllen</a>
+                        </Card>
+
+                        <Card className={'contact__card'}>
+                            <AiOutlineFieldTime className='contact__option-icon' />
+                            <h4>Extra Zeit</h4>
+                            <h5>Formular</h5>
+                            <a href="/en" target="__blank">Füllen</a>
+                        </Card>
+                    </div>
+                </div>
+
+            </div>
         </>
     )
 }
